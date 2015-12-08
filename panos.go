a0ec339f879e23a5c1de31644df77e7992bb15c3
Scott Ware <scottdware@gmail.com>
2015-12-07 22:13:08 -0500

    Added go-requestor for all REST calls

--- panos.go
+++ panos.go
@@ -2,36 +2,22 @@
 package panos
 
 import (
-	"bytes"
-	"crypto/tls"
 	"encoding/xml"
 	"errors"
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"strings"
+	"github.com/scottdware/go-requestor"
 )
 
 // PaloAlto is a container for our session state.
 type PaloAlto struct {
-	Host      string
-	Key       string
-	Transport *http.Transport
+	Host string
+	Key  string
 }
 
 // authKey holds our API key.
 type authKey struct {
 	XMLName xml.Name `xml:"response"`
 	Key     string   `xml:"result>key"`
-}
-
-// APIRequest builds our request before sending it to the server.
-type APIRequest struct {
-	Method string
-	URL    string
-	Body   string
-	Action string
-	XPath  string
 }
 
 // requestError contains information about any error we get from a request.
@@ -43,51 +29,21 @@ type requestError struct {
 // NewSession sets up our connection to the Palo Alto firewall system.
 func NewSession(host, user, passwd string) *PaloAlto {
 	var key authKey
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{
-			InsecureSkipVerify: true,
-		},
+
+	resp := requestor.Send(fmt.Sprintf("https://%s/api/?type=keygen&user=%s&password=%s", host, user, passwd), nil)
+	if resp.Error != nil {
+		fmt.Println(resp.Error)
 	}
 
-	client := &http.Client{Transport: tr}
-	resp, err := client.Get(fmt.Sprintf("https://%s/api/?type=keygen&user=%s&password=%s", host, user, passwd))
+	err := xml.Unmarshal(resp.Body, &key)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	data, _ := ioutil.ReadAll(resp.Body)
-	xml.Unmarshal(data, &key)
-
 	return &PaloAlto{
 		Host: host,
 		Key:  key.Key,
-		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{
-				InsecureSkipVerify: true,
-			},
-		},
 	}
-}
-
-// APICall is used to query the Palo Alto API.
-func (p *PaloAlto) APICall(options *APIRequest) ([]byte, error) {
-	var req *http.Request
-	client := &http.Client{Transport: p.Transport}
-	url := fmt.Sprintf("https://%s/api/?type=config&key=%s&action=%s&xpath=%s", p.Host, p.Key, options.Action, options.XPath)
-	body := bytes.NewReader([]byte(options.Body))
-	req, _ = http.NewRequest(strings.ToUpper(options.Method), url, body)
-	req.Header.Set("Content-Type", "application/xml")
-
-	res, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-
-	defer res.Body.Close()
-
-	data, _ := ioutil.ReadAll(res.Body)
-
-	return data, nil
 }
 
 // checkError handles any errors we get from our API requests. It returns either the
