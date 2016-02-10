9d96136c969113f8d4c4410a9cddaa79900dc7b3
Scott Ware <scottdware@gmail.com>
2016-02-10 10:37:15 -0500

    Added group type

--- address.go
+++ address.go
@@ -4,6 +4,7 @@ import (
 	"encoding/xml"
 	"fmt"
 	"github.com/scottdware/go-rested"
+	"strings"
 )
 
 // AddressObjects contains a slice of all address objects.
@@ -22,14 +23,30 @@ type Address struct {
 
 // AddressGroups contains a slice of all address groups.
 type AddressGroups struct {
-	XMLName xml.Name       `xml:"response"`
-	Groups  []AddressGroup `xml:"result>address-group>entry"`
+	Groups []AddressGroup
 }
 
 // AddressGroup contains information about each individual address group.
 type AddressGroup struct {
-	Name         string   `xml:"name,attr"`
-	StaticMember []string `xml:"static>member,omitempty"`
+	Name          string
+	Type          string
+	Members       []string
+	DynamicFilter string
+	Description   string
+}
+
+// xmlAddressGroups is used for parsing of all address groups.
+type xmlAddressGroups struct {
+	XMLName xml.Name          `xml:"response"`
+	Groups  []xmlAddressGroup `xml:"result>address-group>entry"`
+}
+
+// xmlAddressGroup is used for parsing each individual address group.
+type xmlAddressGroup struct {
+	Name          string   `xml:"name,attr"`
+	Members       []string `xml:"static>member,omitempty"`
+	DynamicFilter string   `xml:"dynamic>filter,omitempty"`
+	Description   string   `xml:"description,omitempty"`
 }
 
 // Addresses returns information about all of the address objects.
@@ -62,6 +79,7 @@ func (p *PaloAlto) Addresses() *AddressObjects {
 
 // AddressGroups returns information about all of the address groups.
 func (p *PaloAlto) AddressGroups() *AddressGroups {
+	var parsedGroups xmlAddressGroups
 	var groups AddressGroups
 	r := rested.NewRequest()
 
@@ -81,8 +99,22 @@ func (p *PaloAlto) AddressGroups() *AddressGroups {
 	}
 	groupData := r.Send("get", p.URI, nil, headers, query)
 
-	if err := xml.Unmarshal(groupData.Body, &groups); err != nil {
+	if err := xml.Unmarshal(groupData.Body, &parsedGroups); err != nil {
 		fmt.Println(err)
+	}
+
+	for _, g := range parsedGroups.Groups {
+		gname := g.Name
+		gtype := "Static"
+		gmembers := g.Members
+		gfilter := strings.TrimSpace(g.DynamicFilter)
+		gdesc := g.Description
+
+		if g.DynamicFilter != "" {
+			gtype = "Dynamic"
+		}
+
+		groups.Groups = append(groups.Groups, AddressGroup{Name: gname, Type: gtype, Members: gmembers, DynamicFilter: gfilter, Description: gdesc})
 	}
 
 	return &groups
