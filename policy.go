c909665c8d27cf7f18433aee5e6fecf3fdeefce2
Scott Ware <USZ0JCP@amer.zurich.corp>
2018-06-25 11:54:50 -0400

    Added function to create a security rule.

--- policy.go
+++ policy.go
@@ -29,27 +29,82 @@ type policyRules struct {
 // Rule contains information about each individual security rule.
 type Rule struct {
 	Name                 string   `xml:"name,attr"`
-	From                 []string `xml:"from>member"`
-	To                   []string `xml:"to>member"`
-	Source               []string `xml:"source>member"`
-	Destination          []string `xml:"destination>member"`
-	SourceUser           []string `xml:"source-user>member"`
-	Application          []string `xml:"application>member"`
-	Service              []string `xml:"service>member"`
-	HIPProfiles          []string `xml:"hip-profiles>member"`
-	Category             []string `xml:"category>member"`
-	Action               string   `xml:"action"`
-	LogStart             string   `xml:"log-start"`
-	LogEnd               string   `xml:"log-end"`
-	Tag                  []string `xml:"tag>member"`
-	LogSetting           string   `xml:"log-setting"`
-	URLFilteringProfile  string   `xml:"profile-setting>profiles>url-filtering>member"`
-	FileBlockingProfile  string   `xml:"profile-setting>profiles>file-blocking>member"`
-	AntiVirusProfile     string   `xml:"profile-setting>profiles>virus>member"`
-	AntiSpywareProfile   string   `xml:"profile-setting>profiles>spyware>member"`
-	VulnerabilityProfile string   `xml:"profile-setting>profiles>vulnerability>member"`
-	WildfireProfile      string   `xml:"profile-setting>profiles>wildfire-analysis>member"`
-	SecurityProfileGroup string   `xml:"profile-setting>group>member"`
+	Tag                  []string `xml:"tag>member,omitempty"`
+	From                 []string `xml:"from>member,omitempty"`
+	To                   []string `xml:"to>member,omitempty"`
+	Source               []string `xml:"source>member,omitempty"`
+	Destination          []string `xml:"destination>member,omitempty"`
+	SourceUser           []string `xml:"source-user>member,omitempty"`
+	Application          []string `xml:"application>member,omitempty"`
+	Service              []string `xml:"service>member,omitempty"`
+	HIPProfiles          []string `xml:"hip-profiles>member,omitempty"`
+	Category             []string `xml:"category>member,omitempty"`
+	Action               string   `xml:"action,omitempty"`
+	LogStart             string   `xml:"log-start,omitempty"`
+	LogEnd               string   `xml:"log-end,omitempty"`
+	LogSetting           string   `xml:"log-setting,omitempty"`
+	Disabled             string   `xml:"disabled,omitempty"`
+	URLFilteringProfile  string   `xml:"profile-setting>profiles>url-filtering>member,omitempty"`
+	FileBlockingProfile  string   `xml:"profile-setting>profiles>file-blocking>member,omitempty"`
+	AntiVirusProfile     string   `xml:"profile-setting>profiles>virus>member,omitempty"`
+	AntiSpywareProfile   string   `xml:"profile-setting>profiles>spyware>member,omitempty"`
+	VulnerabilityProfile string   `xml:"profile-setting>profiles>vulnerability>member,omitempty"`
+	WildfireProfile      string   `xml:"profile-setting>profiles>wildfire-analysis>member,omitempty"`
+	SecurityProfileGroup string   `xml:"profile-setting>group>member,omitempty"`
+	Description          string   `xml:"description,omitempty"`
+}
+
+// RuleContent is used to hold the information that will be used
+// to create a new rule
+type RuleContent struct {
+	// Name of the rule.
+	Name string
+	// Tag or tags that you want the rule to be a part of.
+	Tag []string
+	// The source zone.
+	From []string
+	// The destination zone.
+	To []string
+	// The source address.
+	Source []string
+	// The destination address.
+	Destination []string
+	// The source user or users.
+	SourceUser []string
+	// The applications you want to include.
+	Application []string
+	// The services you want to include.
+	Service []string
+	// HIP profiles to check traffic against.
+	HIPProfiles []string
+	// The URL category.
+	Category []string
+	// The action you want to take on the rule.
+	Action string
+	// Log at session start.
+	LogStart string
+	// Log at session end.
+	LogEnd string
+	// The log setting, such as forwarding them to Panorama.
+	LogSetting string
+	// Disable the rule.
+	Disabled string
+	// URL filtering profile.
+	URLFilteringProfile string
+	// File blocking profile.
+	FileBlockingProfile string
+	// Antivirus profile.
+	AntiVirusProfile string
+	// Anti-spyware profile.
+	AntiSpywareProfile string
+	// Vulnderability profile.
+	VulnerabilityProfile string
+	// Wildfire profile.
+	WildfireProfile string
+	// Security profile group.
+	SecurityProfileGroup string
+	// Description (optional)
+	Description string
 }
 
 // NATPolicy contains information about all of the NAT rules on the device.
@@ -91,7 +146,7 @@ func (p *PaloAlto) Policy(devicegroup ...string) (*Policy, error) {
 	case "panos":
 		xpath := "/config/devices/entry[@name='localhost.localdomain']/vsys/entry[@name='vsys1']/rulebase/security/rules"
 
-		if len(devicegroup) == 0 {
+		if len(devicegroup) > 0 {
 			return nil, errors.New("you do not need to specify a device-group when connected to a fireawll")
 		}
 
@@ -199,4 +254,179 @@ func (p *PaloAlto) NATPolicy() (*NATPolicy, error) {
 	}
 
 	return &policy, nil
+}
+
+// CreateRule will create a new rule on the device. If you are connected to a Panorama device, then
+// you must specify the device-group as the last parameter. You do not need this when connected to
+// a firewall.
+//
+// Ruletype must be one of:
+//
+// pre, post, local (only if used on a firewall)
+//
+// You will need to create the rules contents within the RuleContent struct. Please see the documentation
+// for the struct on how to structure it.
+func (p *PaloAlto) CreateRule(name, ruletype string, content *RuleContent, devicegroup ...string) error {
+	var xmlBody string
+	var reqError requestError
+	var xpath string
+
+	urlp := len(content.URLFilteringProfile)
+	fp := len(content.FileBlockingProfile)
+	wfp := len(content.WildfireProfile)
+	avp := len(content.AntiVirusProfile)
+	asp := len(content.AntiSpywareProfile)
+	vp := len(content.VulnerabilityProfile)
+
+	if p.DeviceType == "panos" {
+		xpath = fmt.Sprintf("/config/devices/entry[@name='localhost.localdomain']/vsys/entry[@name='vsys1']/rulebase/security/rules/entry[@name='%s']", name)
+	}
+
+	if p.DeviceType == "panorama" && len(devicegroup) == 0 {
+		return errors.New("you must specify a device-group when creating a rule on a Panorama device")
+	}
+
+	if len(devicegroup) > 0 {
+		switch ruletype {
+		case "pre":
+			xpath = fmt.Sprintf("/config/devices/entry[@name='localhost.localdomain']/device-group/entry[@name='%s']/pre-rulebase/security/rules/entry[@name='%s']", devicegroup[0], name)
+		case "post":
+			xpath = fmt.Sprintf("/config/devices/entry[@name='localhost.localdomain']/device-group/entry[@name='%s']/post-rulebase/security/rules/entry[@name='%s']", devicegroup[0], name)
+		}
+	}
+
+	if len(content.Tag) > 0 {
+		xmlBody += fmt.Sprintf("<tag>")
+		for _, tag := range content.Tag {
+			xmlBody += fmt.Sprintf("<member>%s</member>", tag)
+		}
+		xmlBody += fmt.Sprintf("</tag>")
+	}
+
+	xmlBody += fmt.Sprintf("<to>")
+	for _, to := range content.To {
+		xmlBody += fmt.Sprintf("<member>%s</member>", to)
+	}
+	xmlBody += fmt.Sprintf("</to>")
+
+	xmlBody += fmt.Sprintf("<from>")
+	for _, from := range content.From {
+		xmlBody += fmt.Sprintf("<member>%s</member>", from)
+	}
+	xmlBody += fmt.Sprintf("</from>")
+
+	xmlBody += fmt.Sprintf("<source>")
+	for _, source := range content.Source {
+		xmlBody += fmt.Sprintf("<member>%s</member>", source)
+	}
+	xmlBody += fmt.Sprintf("</source>")
+
+	xmlBody += fmt.Sprintf("<destination>")
+	for _, destination := range content.Destination {
+		xmlBody += fmt.Sprintf("<member>%s</member>", destination)
+	}
+	xmlBody += fmt.Sprintf("</destination>")
+
+	xmlBody += fmt.Sprintf("<source-user>")
+	for _, srcuser := range content.SourceUser {
+		xmlBody += fmt.Sprintf("<member>%s</member>", srcuser)
+	}
+	xmlBody += fmt.Sprintf("</source-user>")
+
+	xmlBody += fmt.Sprintf("<category>")
+	for _, category := range content.Category {
+		xmlBody += fmt.Sprintf("<member>%s</member>", category)
+	}
+	xmlBody += fmt.Sprintf("</category>")
+
+	xmlBody += fmt.Sprintf("<application>")
+	for _, app := range content.Application {
+		xmlBody += fmt.Sprintf("<member>%s</member>", app)
+	}
+	xmlBody += fmt.Sprintf("</application>")
+
+	xmlBody += fmt.Sprintf("<service>")
+	for _, service := range content.Service {
+		xmlBody += fmt.Sprintf("<member>%s</member>", service)
+	}
+	xmlBody += fmt.Sprintf("</service>")
+
+	xmlBody += fmt.Sprintf("<hip-profiles>")
+	for _, hip := range content.HIPProfiles {
+		xmlBody += fmt.Sprintf("<member>%s</member>", hip)
+	}
+	xmlBody += fmt.Sprintf("</hip-profiles>")
+
+	xmlBody += fmt.Sprintf("<action>%s</action>", content.Action)
+
+	if len(content.LogStart) > 0 {
+		xmlBody += fmt.Sprintf("<log-start>%s</log-start>", content.LogStart)
+	}
+
+	if len(content.LogEnd) > 0 {
+		xmlBody += fmt.Sprintf("<log-end>%s</log-end>", content.LogEnd)
+	}
+
+	if len(content.LogSetting) > 0 {
+		xmlBody += fmt.Sprintf("<log-setting>%s</log-setting>", content.LogSetting)
+	}
+
+	if len(content.SecurityProfileGroup) > 0 {
+		xmlBody += fmt.Sprintf("<profile-setting><group><member>%s</member></group></profile-setting>", content.SecurityProfileGroup)
+	}
+
+	if len(content.Description) > 0 {
+		xmlBody += fmt.Sprintf("<description>%s</description>", content.Description)
+	}
+
+	if len(content.Disabled) > 0 {
+		xmlBody += fmt.Sprintf("<disabled>%s</disabled>", content.Disabled)
+	}
+
+	if urlp > 0 || fp > 0 || wfp > 0 || avp > 0 || asp > 0 || vp > 0 {
+		xmlBody += "<profile-setting><profiles>"
+
+		if urlp > 0 {
+			xmlBody += fmt.Sprintf("<url-filtering><member>%s</member></url-filtering>", content.URLFilteringProfile)
+		}
+
+		if fp > 0 {
+			xmlBody += fmt.Sprintf("<file-blocking><member>%s</member></file-blocking>", content.FileBlockingProfile)
+		}
+
+		if wfp > 0 {
+			xmlBody += fmt.Sprintf("<wildfire-analysis><member>%s</member></wildfire-analysis>", content.WildfireProfile)
+		}
+
+		if avp > 0 {
+			xmlBody += fmt.Sprintf("<virus><member>%s</member></virus>", content.AntiVirusProfile)
+		}
+
+		if asp > 0 {
+			xmlBody += fmt.Sprintf("<spyware><member>%s</member></spyware>", content.AntiSpywareProfile)
+		}
+
+		if vp > 0 {
+			xmlBody += fmt.Sprintf("<vulnerability><member>%s</member></vulnerability>", content.VulnerabilityProfile)
+		}
+
+		xmlBody += "</profiles></profile-setting>"
+	}
+
+	fmt.Println(xmlBody)
+
+	_, resp, errs := r.Post(p.URI).Query(fmt.Sprintf("type=config&action=set&xpath=%s&element=%s&key=%s", xpath, xmlBody, p.Key)).End()
+	if errs != nil {
+		return errs[0]
+	}
+
+	if err := xml.Unmarshal([]byte(resp), &reqError); err != nil {
+		return err
+	}
+
+	if reqError.Status != "success" {
+		return fmt.Errorf("error code %s: %s", reqError.Code, errorCodes[reqError.Code])
+	}
+
+	return nil
 }
